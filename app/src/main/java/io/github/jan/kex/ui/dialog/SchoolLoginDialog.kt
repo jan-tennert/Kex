6cdcf9640b60bca3d7c7b700ca6859362f4b3498
Jan <jan.m.tennert@gmail.com>
2023-09-01 22:49:05 +0200

    add missing import

--- app/src/main/java/io/github/jan/kex/ui/dialog/SchoolLoginDialog.kt
+++ app/src/main/java/io/github/jan/kex/ui/dialog/SchoolLoginDialog.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.TextButton
