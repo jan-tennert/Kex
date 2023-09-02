05ec6b7fbad56b16af6a515147523f34edb1a4cd
Jan Tennert <jan.m.tennert@gmail.com>
2023-09-03 01:29:25 +0200

    remove ksp

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@
 plugins {
     alias(libs.plugins.com.android.application) apply false
     alias(libs.plugins.org.jetbrains.kotlin.android) apply false
-    alias(libs.plugins.ksp) apply false
     alias(libs.plugins.sqldelight) apply false
     alias(libs.plugins.kotlinx.serialization)
 }
