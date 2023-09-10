188b7deaf4849af3687f6b4940d7ecfe7a8276e4
TheRealJan <jan.m.tennert@gmail.com>
2023-09-10 19:55:39 +0200

    fix home screen

--- app/src/main/java/io/github/jan/kex/ui/screen/HomeScreen.kt
+++ app/src/main/java/io/github/jan/kex/ui/screen/HomeScreen.kt
@@ -72,7 +72,7 @@ fun HomeScreen(
                 }
             }
             Box(Modifier.padding(8.dp)) {
-                VerticalDivider(Modifier.fillMaxWidth())
+                VerticalDivider(Modifier.fillMaxHeight())
             }
             LazyColumn(
                 modifier = Modifier
@@ -91,7 +91,7 @@ fun HomeScreen(
                 }
             }
             Box(Modifier.padding(8.dp)) {
-                HorizontalDivider(Modifier.fillMaxHeight())
+                HorizontalDivider(Modifier.fillMaxWidth())
             }
             LazyColumn(Modifier.fillMaxHeight().fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
                 taskList(tasksByDays, taskVm::updateTask, taskVm::deleteTask)
